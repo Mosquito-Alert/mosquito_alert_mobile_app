4b6923f99a2fde949756311cc991d0f53f23299e
Enric Pou <enricpou@gmail.com>
2026-05-13 10:40:26 +0200

    Migrate onboarding old system on init

--- lib/features/onboarding/data/onboarding_repository.dart
+++ lib/features/onboarding/data/onboarding_repository.dart
@@ -1,17 +1,22 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class OnboardingRepository {
-  Future<bool> isCompleted() async {
-    final prefs = await SharedPreferences.getInstance();
+  OnboardingRepository() {
+    _init();
+  }
 
+  Future<void> _init() async {
+    final prefs = await SharedPreferences.getInstance();
     // Migrate old onboarding format if necessary
     final oldCompleted = prefs.getBool('firstTime');
     if (oldCompleted != null) {
       await prefs.setBool('onboarding_completed', oldCompleted);
       await prefs.remove('firstTime');
-      return oldCompleted;
     }
+  }
 
+  Future<bool> isCompleted() async {
+    final prefs = await SharedPreferences.getInstance();
     return prefs.getBool('onboarding_completed') ?? false;
   }
 
